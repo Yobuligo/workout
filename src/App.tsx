c368186ec47281eaa02444aff61f189c09914e59
Yobuligo <hoffmannpeter82@gmail.com>
2023-10-17 22:18:27 +0200

    Add example for LocalStorageDb

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import React, { useEffect } from "react";
 import "./App.css";
+import { IDataObject } from "./localStorage/dataObject/IDataObject";
 import { Database } from "./localStorage/database/Database";
 import { IPerson } from "./localStorage/localStorage";
 
+interface ICar extends IDataObject {
+  name: string;
+  power: number;
+}
+
 const App: React.FC = () => {
-  useEffect(() => {    const db = new Database("retrospective");
+  useEffect(() => {
+    const db = new Database("retrospective2");
     const Person = db.create<IPerson>("/users");
     const data = Person.findAll();
-    const person = Person.insert({
-      age: 123,
-      firstname: "Stacey",
-      lastname: "Starfish",
-    });
-    const persons = Person.findAll();
+    // const person = Person.insert({
+    //   age: 123,
+    //   firstname: "Stacey",
+    //   lastname: "Starfish",
+    // });
+    // const persons = Person.findAll();
+    const Car = db.create<ICar>("cars");
+    const cars = Car.findAll();
+    debugger;
+    // Car.insert({ name: "BMW", power: 200 });
   }, []);
 
   return <>Hello World</>;
