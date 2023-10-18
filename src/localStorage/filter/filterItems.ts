8c2fd0554936102f48d2ebd69acdd5f1152fc903
Peter <hoffmannpeter82@gmail.com>
2023-10-18 23:18:03 +0200

    Add documentation

--- src/localStorage/filter/filterItems.ts
+++ src/localStorage/filter/filterItems.ts
@@ -2,11 +2,33 @@ import { error } from "../../utils/error/error";
 import { IDataObject } from "../dataObject/IDataObject";
 import { IFilter } from "./IFilter";
 
-export const filterItems = <T>(items: T[], filter: IFilter<T>): T[] => {
+export const filterItems = <T extends IDataObject>(
+  items: T[],
+  filter: IFilter<T>
+): T[] => {
   return items.filter((item) => doesMatchFilter(item, filter));
 };
 
-export const reduceItems = <T>(items: T[], filter: IFilter<T>): T[] => {
+/**
+ * This function is responsible for reducing the given {@link items} by all entries,
+ * which are matching the given {@link filter} and returning the result list.
+ *
+ * @example
+ * const result = reduceItems(
+ *   [
+ *     { id: 1, name: "Stacey" },
+ *     { id: 2, name: "Alex" },
+ *   ],
+ *   { name: eq("Stacey") }
+ * );
+ *
+ * // result would be
+ * [{ id: 2, name: "Alex" }];
+ */
+export const reduceItems = <T extends IDataObject>(
+  items: T[],
+  filter: IFilter<T>
+): T[] => {
   return items.filter((item) => !doesMatchFilter(item, filter));
 };
 
@@ -30,7 +52,10 @@ export const updateItem = <T>(item: T, origin: T): boolean => {
   return updated;
 };
 
-export const doesMatchFilter = <T>(item: T, filter: IFilter<T>): boolean => {
+export const doesMatchFilter = <T extends IDataObject>(
+  item: T,
+  filter: IFilter<T>
+): boolean => {
   for (const key in filter) {
     const predicate = filter[key] ?? error();
     if (!predicate(item[key])) {
