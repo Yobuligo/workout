08bfceb415aa74b5e4807f8da9bf48fef2d1a7ad
Peter <hoffmannpeter82@gmail.com>
2023-10-24 10:22:39 +0200

    Implement MemoryStorage

---
+++ src/localStorage2/storage/MemoryStorage.ts
@@ -0,0 +1,32 @@
+import { IStorage } from "./IStorage";
+
+export class MemoryStorage<T> implements IStorage<T> {
+  private data: T[] = [];
+
+  constructor(private readonly key: string) {}
+
+  append(item: T): void;
+  append(items: T[]): void;
+  append(items: unknown): void {
+    if (Array.isArray(items)) {
+      if (items.length === 0) {
+        return;
+      }
+      this.data.push(...items);
+    } else {
+      this.data.push(items as T);
+    }
+  }
+
+  delete(): void {
+    this.data = [];
+  }
+
+  read(): T[] {
+    return this.data;
+  }
+
+  write(items: T[]): void {
+    this.data = items;
+  }
+}
