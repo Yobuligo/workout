3ba003523ba235f64fea46e2f98bcf6a9f05017b
Peter <hoffmannpeter82@gmail.com>
2023-10-24 10:17:37 +0200

    Remove dataObject

--- src/localStorage2/dataObject/IDataObject.ts
+++
@@ -1,8 +0,0 @@
-import { IRecord } from "../record/IRecord";
-import { IdType } from "../types/IdType";
-
-/**
- * An implementation of this interface represents a data object which refers to a specific entity.
- * E.g. Person might be a data object type.
- */
-export interface IDataObject<T extends IdType> extends IRecord<T> {}
