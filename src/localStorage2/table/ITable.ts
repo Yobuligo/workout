1e86cd10b43865a4ee39d5592f85df12a87d9418
Peter <hoffmannpeter82@gmail.com>
2023-10-24 10:13:46 +0200

    Adjust ITable by restrict ids to IdType

--- src/localStorage2/table/ITable.ts
+++ src/localStorage2/table/ITable.ts
@@ -1,12 +1,13 @@
 import { IRecord } from "../record/IRecord";
 import { IRecordDetails } from "../record/IRecordDetails";
+import { IdType } from "../types/IdType";
 import { IWhere } from "../where/IWhere";
 import { IUpdateResult } from "./IUpdateResult";
 
 /**
  * An implementation of this interface represents a table which can have data of type {@link TRecord}.
  */
-export interface ITable<TRecord extends IRecord<any>> {
+export interface ITable<TRecord extends IRecord<IdType>> {
   readonly name: string;
   count(): number;
   delete(where?: IWhere<TRecord>): void;
